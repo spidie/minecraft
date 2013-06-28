33763c57a35698bd9c7883ac6cb4764ed03b4e36
mc <mc@treefrog.(none)>
2013-06-28 19:52:40 +1000

    magic

--- client/minecraftsp.sh
+++ client/minecraftsp.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+cd "$(dirname "$0")"
 export LD_LIBRARY_PATH="/usr/lib/jvm/java-7-oracle/jre/lib/amd64"
 java -Xmx1024M -Xms512M -cp MinecraftSP.jar net.minecraft.LauncherFrame
--- magiclauncher/magiclauncher.sh
+++ magiclauncher/magiclauncher.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+cd "$(dirname "$0")"
 export LD_LIBRARY_PATH="/usr/lib/jvm/java-7-oracle/jre/lib/amd64"
 java -Xmx2048M -Xms512M -jar magiclauncher.jar
